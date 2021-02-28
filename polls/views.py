8b89ff0af7a7d83c3e0ee6a39b20e9f2c1674540
“miyamiyasan” <miyakoshi0114@icloud.com>
2021-02-28 11:59:53 +0900

    2 commit

--- polls/views.py
+++ polls/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello,world")
+    return HttpResponse("Hello,world !!!")
     
 # Create your views here.
